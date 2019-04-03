f4f44c27a53c9eabc18b411ffa95be44314014a9
Imanol Fernandez <mortimergoro@gmail.com>
2019-04-03 17:00:45 +0200

    Update keyboard theme

--- app/src/common/shared/org/mozilla/vrbrowser/ui/views/CustomKeyboardView.java
+++ app/src/common/shared/org/mozilla/vrbrowser/ui/views/CustomKeyboardView.java
@@ -348,7 +348,7 @@ public class CustomKeyboardView extends View implements View.OnClickListener {
 
         resetMultiTap();
 
-        mForegroundColor = context.getColor(R.color.fog);
+        mForegroundColor = context.getColor(R.color.asphalt);
         mSelectedForegroundColor = context.getColor(R.color.fog);
     }
 
@@ -787,6 +787,13 @@ public class CustomKeyboardView extends View implements View.OnClickListener {
                 statePadding = mKeyboardPressedPadding;
             }
 
+            int targetColor = mKeyTextColor;
+            if (stateHovered) {
+                targetColor = mForegroundColor;
+            } else if (statePressed) {
+                targetColor = mSelectedForegroundColor;
+            }
+
             if (label != null) {
                 float descent = paint.descent();
 
@@ -800,15 +807,7 @@ public class CustomKeyboardView extends View implements View.OnClickListener {
                     paint.setTextSize(mKeyTextSize);
                     paint.setTypeface(Typeface.DEFAULT);
                 }
-                if (!stateHovered && !statePressed) {
-                    paint.setColor(mKeyTextColor);
-
-                } else if (stateHovered) {
-                    paint.setColor(mForegroundColor);
-
-                } else if (statePressed) {
-                    paint.setColor(mSelectedForegroundColor);
-                }
+                paint.setColor(targetColor);
 
                 // Draw a drop shadow for the text
                 paint.setShadowLayer(mShadowRadius, 0, 0, mShadowColor);
@@ -829,7 +828,7 @@ public class CustomKeyboardView extends View implements View.OnClickListener {
                 final float drawableY = (key.height - padding.top - padding.bottom - key.icon.getIntrinsicHeight()) / 2
                         + padding.top + statePadding;
                 canvas.translate(drawableX, drawableY);
-                key.icon.setColorFilter(key.pressed ? mSelectedForegroundColor : mForegroundColor, PorterDuff.Mode.MULTIPLY);
+                key.icon.setColorFilter(targetColor, PorterDuff.Mode.MULTIPLY);
                 key.icon.setBounds(0, 0, key.icon.getIntrinsicWidth(), key.icon.getIntrinsicHeight());
                 key.icon.draw(canvas);
                 canvas.translate(-drawableX, -drawableY);
